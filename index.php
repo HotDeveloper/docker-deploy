c233ca50b85c52a1b2d201061be57c1242217a6d
HotDeveloper <cursosdevelopment01@gmail.com>
2026-02-17 02:29:47 +0200

    Update index.php

--- index.php
+++ index.php
@@ -303,7 +303,7 @@
                 </div>
             </div>
             <div class="footer-bottom">
-                <p>&copy; 2026 Consultório Clínico Saúde e Vida Longa. Todos os direitos reservados.</p>
+                <p>&copy; 2026 Consultório Clínico Saúde e Vida Longa. Todos os direitos reservados. Developer</p>
             </div>
         </div>
     </footer>
